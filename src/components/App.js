10532eca62cdddee4cb139920c0ce92ab1ab0523
chrispaul20191610 <chrispaul20191610.christian.llumiquinga@epn.edu.ec>
2021-02-19 23:37:54 -0500

    fcambio usuarios

--- src/components/App.js
+++ src/components/App.js
@@ -1,58 +1,49 @@
-
 import React, {useEffect, useState} from 'react';
 import '../styles/App.css';
 
 function App (){
-    const [list , setList ]=useState ([]);
-    const [userId, setUserId]=useState (1);
-    const [userData, setUserData]=useState(null);
+    const [ list, setList ] = useState( [] );
+  const [ userId, setUserId ] = useState( 1 );
+  const [ userData, setUserData ] = useState( null );
 
+  useEffect( () => {
+    const getUser = async() => {
+      const data = await fetch( `https://jsonplaceholder.typicode.com/users/${ userId }` );
+      const jsonUser = await data.json();
+      console.log( 'user', jsonUser );
 
-    useEffect(()=>{
-        const getUser = async() => {
-            const data = await fetch (`https://jsonplaceholder.typicode.com/users/${ userId }`);
-            const jsonUser = await data.json();
-            console.log('user', jsonUser);
-
-            setUserData (jsonUser);
-
+      setUserData( jsonUser );
 
         };
         getUser();
 
-        const getList=async()=>{
-            const data = await fetch(`https://jsonplaceholder.typicode.com/users/${ userId }/todos`);
-            const jsonList = await data.json;
-            console.log ('list', jsonList);
-
-            setList (jsonList);
+        const getList = async() => {
+            const data = await fetch( `https://jsonplaceholder.typicode.com/users/${ userId }/todos` );
+            const jsonList = await data.json();
+            console.log( 'list', jsonList );
+      
+            setList( jsonList );
 
         };
         getList();
     }, [userId]);
 
     const handleAddTask = () => {
-        const title = document.querySelector ('#task').value;
-        setList(prevState => [
-            ...prevState, {
-                title,
-                completed: false
+        const title = document.querySelector( '#task' ).value;
+        setList( prevState => [
+          ...prevState, {
+            title,
+            completed: false
+          }
+        ] );
+        document.querySelector( '#task' ).value = '';
+      };
+      const handleDeleteTask = ( index ) => {
+        setList( ( prevState ) => {
+          return prevState.filter( ( task, i ) => i !== index );
+        } );
 
-            }
-        ]);
-        document.querySelector('#task').value=' ';
-
-
-    };
-        const handleDeleteTask = (index) => {
-            setList ((prevState) => {
-                return prevState.filter((task,i)=>1!==index);
-
-            }
-            
-            );
-
-        };
+      };
 
         const handleCompleteTask = (index) => {
             setList ((prevState) => {
@@ -64,22 +55,22 @@ function App (){
         };
 
         const handlePrevUser = () => {
-            setUserId (userId-1);
-        };
+            setUserId( userId - 1 );
+          };
 
         const handleNextUser = () => {
-            setUserId (userId+1);
+            setUserId( userId + 1 );
         };
 
         return (
             <>
                 {
                     userId > 1 &&
-                    <button onClick={ handlePrevUser}>anterior </button>
+                    <button onClick={ handlePrevUser }>Anterior usuario</button>
                 }
                 {
-                    userId > 10 && 
-                    <button onClick={ handleNextUser }>siguiente</button>
+                    userId < 10 &&
+                    <button onClick={ handleNextUser }>Siguiente usuario</button>
                 }
                 {
                     userData 
@@ -99,8 +90,26 @@ function App (){
                     :"por favor espere  ...... "
 
                 }
+
+                <div>
+                    <label htmlFor="task">Tareas</label>
+                    <input type="text" id="task"></input>
+                </div>
+                <button onClick={handleAddTask}>nueva tarea</button>
+
+                <h1>Lista de tareas ({ list.length } en total)</h1>
+                 <table>
+                     <thead>
+                        <tr>
+                                <th>Nombre</th>
+                                <th>Estado</th>
+                                <th>Eliminar</th>
+                        </tr>
+                    </thead>
+            
+      </table>
             </>
-        )
+        );
 
 
 
